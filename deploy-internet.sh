40b0e35683d8f8fb4df69686e6f9c0854460331b
Marc Sances <marcsances@gmail.com>
2024-04-27 18:29:45 +0200

    remove scripts

--- deploy-internet.sh
+++
@@ -1,6 +0,0 @@
-rm -rf _site
-bundle exec jekyll build
-cd _site
-tar czvf package.tar.gz *
-scp -P 1310 package.tar.gz marcsances@marquii.duckdns.org:/home/marcsances/ponentdnb/package.tar.gz
-ssh -o StrictHostKeyChecking=no -p 1310 marcsances@marquii.duckdns.org "cd /home/marcsances/ponentdnb; tar zxvf package.tar.gz"
--- deploy-lan.sh
+++
@@ -1,6 +0,0 @@
-rm -rf _site
-bundle exec jekyll build
-cd _site
-tar czvf package.tar.gz *
-scp -P 131 package.tar.gz marcsances@192.168.0.20:/home/marcsances/ponentdnb/package.tar.gz
-ssh -o StrictHostKeyChecking=no -p 131 marcsances@192.168.0.20 "cd /home/marcsances/ponentdnb; tar zxvf package.tar.gz"
